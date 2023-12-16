a65497f7f5ea8da24c43e6ad416154f56d054135
Shailenkaushik <107068730+Shailenkaushik@users.noreply.github.com>
2023-12-16 23:09:16 +0530

    express

--- HTTPSERVER-01/index.js
+++ HTTPSERVER-01/index.js
@@ -1,24 +1,67 @@
 const express = require('express')
 const app = express()
 const port = 3000
+app.use(express.json())
+
+let User=[{
+  name:"john",
+      Kidney:[{
+        healthy:false
+      },
+      {
+      healthy:true
+      }]
+}]
 
 
-function calc(n){
-  let ans=0;
-  for(let i=0;i<=n;i++){
-    ans+=i;
-  }
-  return ans;
-}
+  app.get('/', (req, res) => {
 
+      const userarray=User[0].Kidney;
+      let noofkidney=userarray.length;
+      const healthykidneyarray=userarray.filter(function(obj){
+            if(obj.healthy) return true;
+        return false;
+      })
 
-app.get('/how', (req, res) => {
+      const healthykidney=healthykidneyarray.length;
+        const unhealthykidney=noofkidney-healthykidney;
+      res.json({noofkidney,healthykidney,unhealthykidney});
 
-  let n=req.query.n;
-  let a=calc(n);
-  res.send(a.toString());
+  })
+
+app.post('/', (req, res) => {
+
+     const ishelth=req.body.health;
+    //  console.log(User[0].Kidney.length);
+     User[0].Kidney.push({
+      healthy:ishelth
+     })
+
+    //  console.log(User[0].Kidney.length);
+     res.json({ 
+      msg:"done"
+     })
+
 })
+app.put('/', (req, res) => {
 
+  for(let i=0;i<User[0].Kidney.length;i++){
+    User[0].Kidney[i]=true;
+  }
+  res.json({})
+})
+app.delete('/', (req, res) => {
+    
+  const newarr=User[0].Kidney.filter(function(obj){
+    if(obj.healthy) return true;
+    return false;
+  })
+  User[0].Kidney=newarr;
+  res.status(321).json({
+    msg:"done deletion"
+  })
+
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
